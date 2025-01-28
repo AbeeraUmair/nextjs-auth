1c2bc4334226a684044dd0ff82f272a30090d263
AbeeraUmair <aleemabeera@gmail.com>
2025-01-28 18:16:56 +0500

    UPDATED

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,15 +23,15 @@ export default function LoginPage() {
   };
 
   return (
-    <div>
-      <h1 className="text-4xl">Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="email" name="email" placeholder="Email" required />
-        <input type="password" name="password" placeholder="Password" required />
-        <button type="submit">Login</button>
+    <div className="flex flex-col items-center justify-center mt-32 bg-gray-400 p-20 w-96 rounded-lg mx-auto">
+      <h1 className="text-4xl mb-5">Login</h1>
+      <form className="flex flex-col justify-center items-center mb-5 " onSubmit={handleSubmit}>
+        <input className="mb-5 p-2 rounded-lg" type="email" name="email" placeholder="Email" required />
+        <input className="mb-5 p-2 rounded-lg" type="password" name="password" placeholder="Password" required />
+        <button className="bg-gray-800 text-white  rounded-lg px-4 py-1" type="submit">Login</button>
       </form>
       <p>
-        Dont have an account? <Link href="/register">Register here</Link>
+        Dont have an account? <br/><Link href="/register">Register here</Link>
       </p>
     </div>
   );
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -32,12 +32,12 @@ export default function RegisterPage() {
   };
 
   return (
-    <div>
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="email" name="email" placeholder="Email" required />
-        <input type="password" name="password" placeholder="Password" required />
-        <button type="submit">Register</button>
+    <div className="flex flex-col items-center justify-center mt-32 bg-gray-400 p-20 w-96 rounded-lg mx-auto">
+      <h1 className="text-4xl mb-5">Register</h1>
+      <form className="flex flex-col justify-center items-center mb-5 "  onSubmit={handleSubmit}>
+        <input className="mb-5 p-2 rounded-lg"  type="email" name="email" placeholder="Email" required />
+        <input className="mb-5 p-2 rounded-lg"  type="password" name="password" placeholder="Password" required />
+        <button className="bg-gray-800 text-white  rounded-lg px-4 py-1" type="submit">Register</button>
       </form>
     </div>
   );
